a9485565bacdb11a961960c5fd6a23f946d61554
JSobirov <jasurbey2@gmail.com>
2023-09-27 19:21:43 +0500

    Project layers created

---
+++ Cartopia.DAL/Contexts/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cartopia.DAL.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+
+}
