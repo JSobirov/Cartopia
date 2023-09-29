e5c9a0a829d2bd750bf6c4a035a53c7ecc3c5936
JSobirov <jasurbey2@gmail.com>
2023-09-29 17:10:11 +0500

    DbContext added

--- Cartopia/Program.cs
+++ Cartopia/Program.cs
@@ -1,11 +1,16 @@
+using Cartopia.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
