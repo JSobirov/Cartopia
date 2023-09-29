2ac678c1df977baa4168f0cd7cd88b8d988f3142
JSobirov <jasurbey2@gmail.com>
2023-09-29 16:28:17 +0500

    Repository added

--- Cartopia.Domain/Commons/Auditable.cs
+++ Cartopia.Domain/Commons/Auditable.cs
@@ -5,4 +5,5 @@ public class Auditable
     public long Id { get; set; }
     public DateTime CreateAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
 }
