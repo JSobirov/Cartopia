d9bf9cc72a0d3e94bb47d69deb642f091ded644f
JSobirov <jasurbey2@gmail.com>
2023-09-29 16:15:19 +0500

    DIstrict model added and code cleaned

---
+++ Cartopia.Domain/Entities/District.cs
@@ -0,0 +1,12 @@
+﻿using Cartopia.Domain.Commons;
+
+namespace Cartopia.Domain.Entities;
+
+public class District : Auditable
+{
+    public string NameUz { get; set; }
+    public string NameOz { get; set; }
+    public string NameRu { get; set; }
+    public long RegionId { get; set; }
+    public Region Region { get; set; }
+}
