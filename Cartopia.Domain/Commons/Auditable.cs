7c1f9489834c5565edb63887f022936d55955c1a
JSobirov <jasurbey2@gmail.com>
2023-09-29 15:17:00 +0500

    Auditable added

---
+++ Cartopia.Domain/Commons/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace Cartopia.Domain.Commons;
+
+public class Auditable
+{
+    public long Id { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
--- Cartopia/Program.cs
+++ Cartopia/Program.cs
@@ -1,6 +1,3 @@
-using Cartopia.DAL.Contexts;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,10 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Add DbContext
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
