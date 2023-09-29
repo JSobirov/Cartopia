4eff1acb6c9ed72ac40ffee439cfa21ded82a90e
JSobirov <jasurbey2@gmail.com>
2023-09-29 15:51:33 +0500

    Credit model added

---
+++ Cartopia.Domain/Entities/Credit.cs
@@ -0,0 +1,12 @@
+﻿namespace Cartopia.Domain.Entities;
+
+public class Credit
+{
+    public long ClientId { get; set; }
+    public Client Client { get; set; }
+
+    public long BasketId { get; set; }
+    public Basket Basket { get; set; }
+
+    public decimal Amount { get; set; }
+}
