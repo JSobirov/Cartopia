98eaf6c748a35b1de8bd078b4c75efc1ade028a8
JSobirov <jasurbey2@gmail.com>
2023-09-29 15:36:19 +0500

    Basket model added

---
+++ Cartopia.Domain/Entities/Basket.cs
@@ -0,0 +1,10 @@
+﻿namespace Cartopia.Domain.Entities;
+
+public class Basket
+{
+    public long ClientId { get; set; }
+    public Client Client { get; set; }
+    
+    public List<Product> Products { get; set; }
+    public long Amount { get; set; }
+}
